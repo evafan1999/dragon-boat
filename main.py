48ac9711d1c4ef51b3132a1a2a5e9cca70577bd7
eva1999 <evafan@hotaimotor.com.tw>
2024-10-28 14:27:37 +0800

    update url

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ class Member(BaseModel):
 
 # 爬取數據
 def scrape_data():
-    url = "https://densuke.biz/list?cd=a23SSfHZzRCeKetW"
+    # url = "https://densuke.biz/list?cd=a23SSfHZzRCeKetW"
+    url = "https://densuke.biz/list?cd=5Jxzf7RLcekrQZnv#google_vignette"
     response = requests.get(url)
 
     if response.status_code == 200:
